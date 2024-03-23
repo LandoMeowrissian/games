b4dddcb52e13cc452167861f3d631e77ccab850e
Jamie Ingalls <jamesmaclay@gmail.com>
2024-03-23 19:31:26 -0400

    whackamole game skeleton complete

--- whackamole/app.js
+++ whackamole/app.js
@@ -4,7 +4,9 @@ const timeLeft =  document.querySelector('#time-left')
 const score = document.querySelector('#score')
 
 let result = 0;
+let timerId = null;
 let hitPosition 
+let currentTime = 60;
 
 
 function randomSquare() {
@@ -12,14 +14,13 @@ function randomSquare() {
     square.classList.remove('mole')
   })
 
-  let randomPosition = squares[Math.floor(Math.random() * 9)]
+  let randomPosition = squares[Math.floor(Math.random() * 15)]
   randomPosition.classList.add('mole');
   console.log(randomPosition)
 
   hitPosition = randomPosition.id
 }
 
-randomSquare()
 
 squares.forEach(square => {
   square.addEventListener('mousedown',() => {
@@ -32,9 +33,20 @@ squares.forEach(square => {
 })
 
 function moveMole() {
-  let timerId = null;
   timerId = setInterval(randomSquare, 1000)
 }
 
-
 moveMole();
+
+function countDown() {
+  currentTime--
+  timeLeft.textContent = currentTime
+
+  if (currentTime == 0) {
+    clearInterval(countDownTimerId)
+    clearInterval(timerId)
+    alert('Game Over! Your Score is ' + result)
+  }
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
